b76d8dcbe328e1dbb17bb99d65b3893c2f7e07b6
Ciao <121960452@qq.com>
2018-11-30 23:34:16 +0800

    一些vue的组件练习，组件之间传值， 组件逐层传值， 传方法，

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
-    <div>
-        <img src="./assets/logo.png" alt="logo">
-        <HelloWorld></HelloWorld>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"/>
+      <List :comments="comments" :deleteComment="deleteComment"/>
     </div>
+  </div>
 
 </template>
 
 <script>
-//引入组件
-import HelloWorld from './components/HelloWord.vue'
-export default {
-    components:{
-      HelloWorld
+  //引入组件
+  import Add from './components/add'
+  import List from './components/list'
+
+  export default {
+    data (){
+      return{
+        comments: [
+          {name: 'ciao',content: "so easy"},
+          {name: "xue", content:"just so so"},
+          {name: "chaoke", content: "Vue's interesting"},
+        ]
+      }
+    },
+    methods:{
+      addComment(comment){
+        this.comments.unshift(comment);
+      },
+      deleteComment(index){
+          this.comments.splice(index,1)
+      }
+    },
+    components: {
+      Add, List
     }
-}
+  }
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@
  * 入口Js ：创建vue实例
  */
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+
 new Vue({
-    el: '#app',
-    components: {
-        App
-    },
-    template: '<App/>'
-
-
-
-})
+  el: "#app",
+  components: {App},
+  template: "<App/>"
+});
