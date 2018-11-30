ba317a44ff16f4c5962e84237c594c03cd00a4ef
Ciao <121960452@qq.com>
2018-11-30 18:40:53 +0800

    更换为IDEA 编辑器

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,15 @@
         <img src="./assets/logo.png" alt="logo">
         <HelloWorld/>
     </div>
-    
+
 </template>
 
 <script>
 //引入组件
 import HelloWorld from './components/HelloWord.vue'
 export default {
-    components: {
-        HelloWorld
-         
+    comments:{
+      HelloWorld
     }
 }
 </script>
