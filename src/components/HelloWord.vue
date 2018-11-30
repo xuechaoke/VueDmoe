65d2a5bc30deac2f43309626569217b83db9ef37
Ciao <121960452@qq.com>
2018-11-30 18:38:27 +0800

    更换为IDEA 编辑器

--- src/components/HelloWord.vue
+++ src/components/HelloWord.vue
@@ -10,7 +10,7 @@ export default {
     //data  可以写对象， 也可以写函数， 但是在组件里面必须写函数
     data (){
         return {
-            msg: "Hello Vue"
+            msg: "Hello Vue 更换为IDEA 编辑器"
         }
     }
 }
@@ -19,7 +19,7 @@ export default {
 <style>
 
     .msg{
-      color: cadetblue   
+      color: cadetblue
     }
 </style>
 
